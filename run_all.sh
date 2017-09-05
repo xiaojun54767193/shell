90ec79acad9ed53896a58a2248d9cb39e4e741ff
xiaojun54767193 <547671930@qq.com>
2017-09-05 12:12:23 +0800

    run all the scripts about group

---
+++ run_all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+nohup /home/group_py/run_gn.sh &
+nohup /home/group_py/run_other.sh &
+nohup /home/group_py/run_rename_tmp.sh &
+nohup /home/group_py/run_rename_xdr.sh &
+nohup /home/group_py/run_s1u.sh &
